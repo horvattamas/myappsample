24f439c08bfe3fc3bfda8e7382202a437d98fa0d
Horvat Tamas <horvattamas1@gmail.com>
2019-04-16 15:33:51 +0200

    Added files

---
+++ script.js
@@ -0,0 +1,33 @@
+function moveNumbers(num){
+	var txt = document.getElementById("broj1").value;
+	txt = txt+ num;
+	document.getElementById("broj1").value=txt;
+}
+
+function moveOperator(op){
+	var place = document.getElementById("operator").innerHTML = op;
+}
+
+function resetuj(){
+	document.getElementById("broj1").value=" ";
+	document.getElementById("broj2").value=" ";
+	document.getElementById("operator").innerHTML=" ";
+	document.getElementById("jednako").innerHTML=" ";
+	document.getElementById("broj3").value=" ";
+	document.getElementById("broj3").type="hidden";
+}
+
+function calcul(){
+	document.getElementById("jednako").innerHTML="=";
+	document.getElementById("broj3").type="text";
+	var operator = document.getElementById("operator").textContent;
+	var br1 = document.getElementById("broj1").value;
+	var br2 = document.getElementById("broj2").value;
+	if(operator == "/" && parseInt(br2, 10)==0){
+		br3 = "Deljenje sa nulom je nemoguce!";
+	}else{
+		var pom = br1+operator+br2;
+		var br3 = eval(pom);		
+	}
+	document.getElementById("broj3").value=br3;
+}
