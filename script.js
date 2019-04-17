c36f1b1431c44157bda07e706f3ec689f45797c2
Horvat Tamas <horvattamas1@gmail.com>
2019-04-17 12:11:58 +0200

    minor changes

--- script.js
+++ script.js
@@ -1,54 +1,83 @@
-var curId;
+/*
+Global variable and function for focus assist.
+*/
+let currentTextboxId;
 function setId(id){
-	curId = id;
+	currentTextboxId = id;
 }
-
-function moveNumbers(num){
-	var test = isFocused();
+/*
+This function takes the number value from the button on the HTML 
+and puts it inside the testbox. It also calls the isFocused method
+to test whether the user has focus on the textboxes. 
+*/
+function moveNumber(number){
+	const test = isFocused();
 	if(test){
-		var elemId = curId;
-		var txt = document.getElementById(elemId).value;
-		txt = txt + num;
-		document.getElementById(elemId).value=txt;
+		let elementValue = document.getElementById(currentTextboxId).value;
+		elementValue = elementValue + number;
+		document.getElementById(currentTextboxId).value=elementValue;
 	}
 }
-
+/*
+Test the user's focus on the textbox, returns true if the user has 
+focus or an alert and false if there is no focus.
+*/
 function isFocused(){
-	var el1 = "broj1";
-	var inFocus = curId;
-	var el2 = "broj2";
-	if(el1===inFocus || el2===inFocus){
+	const element1 = "firstNumber";
+	const element2 = "secondNumber";
+	if(element1 === currentTextboxId || element2 === currentTextboxId){
 		return true;
 	}else{
-		window.alert("Morate fokusirati na textbox.");
+		window.alert("You must focus on a textbox first.");
 		return false;
 	}
 }
-
+/*
+Gets the operator from the button and puts it inside a span.
+*/
 function moveOperator(op){
-	var place = document.getElementById("operator").innerHTML = op;
+	const operator = document.getElementById("operator").innerHTML = op;
 }
-
-function resetuj(){
-	document.getElementById("broj1").value=" ";
-	document.getElementById("broj2").value=" ";
+/*
+Resets every elements value and text to an empty String.
+*/
+function resetCalculator(){
+	document.getElementById("firstNumber").value=" ";
+	document.getElementById("secondNumber").value=" ";
 	document.getElementById("operator").innerHTML=" ";
-	document.getElementById("jednako").innerHTML=" ";
-	document.getElementById("broj3").value=" ";
-	document.getElementById("broj3").type="hidden";
+	document.getElementById("equals").innerHTML=" ";
 }
-
-function calcul(){
-	var operator = document.getElementById("operator").textContent;
-	var br1 = document.getElementById("broj1").value;
-	var br2 = document.getElementById("broj2").value;
-	if(operator == "/" && parseInt(br2, 10)==0){
-		window.alert("Deljenje sa nulom je nemoguce!");
-	}else{
-		document.getElementById("jednako").innerHTML="=";
-		document.getElementById("broj3").type="text";
-		var pom = br1+operator+br2;
-		var br3 = eval(pom);		
+/*
+Gets the numbers from the textboxes and calls upon the calculate(number1,operator,number2)
+function gets the result and writes it on the HTML.
+*/
+function writeResult(){
+	const operator = document.getElementById("operator").textContent;
+	const number1 = document.getElementById("firstNumber").value;
+	const number2 = document.getElementById("secondNumber").value;
+	const resultNumber = calculate(number1, operator, number2);
+	document.getElementById("equals").innerHTML="="+resultNumber;
+}
+/*
+Takes 2 numbers(variable 1 and 3) and an operator all of them as a String,
+parses the numbers into integers and calculates the result.
+*/
+function calculate(number1, operator, number2){
+	const num1 = parseInt(number1, 10);
+	const num2 = parseInt(number2, 10);
+	let result;
+	if(operator === "+"){
+		result = num1 + num2;
+	}else if(operator === "-"){
+		result = num1 - num2;
+	}else if(operator === "*"){
+		result = num1 * num2;
+	}else if(operator === "/"){
+		if(num2 === 0){
+			window.alert("Division by 0 is not possible.");
+		}else{
+			result = num1 / num2;
+		}
 	}
-	document.getElementById("broj3").value=br3;
+	return result;
 }
